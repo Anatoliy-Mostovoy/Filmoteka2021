e43517ee7c18c783523d9e7261d5e0d395bea429
playsoksana <playsoksana@gmail.com>
2021-06-26 22:50:52 +0300

    сняла и добавила обработчики

--- src/js/team.js
+++ src/js/team.js
@@ -2,16 +2,17 @@ import { refs } from './variables';
 const { backdropTeam,  bodyEl, teamClose, linkStudents} = refs;
 
 backdropTeam.addEventListener('click', closeCard);
-linkStudents.addEventListener('click', closeModal); //открыть модалку
+linkStudents.addEventListener('click', closeModal); 
 
 function closeModal() {
-
+  linkStudents.removeEventListener('click', closeModal);
   backdropTeam.classList.remove('backdrop-hidden');
 }
 
 
 
 function closeCard(ev) {
+  backdropTeam.removeEventListener('click', closeCard);
   ev.preventDefault();
  
       const current = document.querySelector('.default.expand');
@@ -21,13 +22,16 @@ function closeCard(ev) {
         
   if (current) {
         ev.target.classList.remove('expand');
-        current.classList.remove('expand');
+    current.classList.remove('expand');
+    
       if (ev.target !== current) {
         ev.target.classList.add('expand');
+        backdropTeam.addEventListener('click', closeCard);
         }
            
     } else {
-        ev.target.classList.add('expand');
+    ev.target.classList.add('expand');
+    backdropTeam.addEventListener('click', closeCard);
     backdropClick(ev);
   }
 }
@@ -38,6 +42,7 @@ function closeModalTeam(ev) {
   backdropTeam.classList.add('backdrop-hidden');
   backdropTeam.removeEventListener('click', backdropClick);
   window.removeEventListener('keydown', onPressEsc);
+  linkStudents.addEventListener('click', closeModal);
    }
 
 function onPressEsc(evt) {
