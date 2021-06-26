e1d90eb418b4758edd2cd9f42bc1c72d28b41abe
Ivan Laver <ereyzer142@gmail.com>
2021-06-26 22:40:24 +0300

    pushchange

--- src/js/local-storage-API.js
+++ src/js/local-storage-API.js
@@ -1,28 +1,16 @@
 import { refs } from './variables';
 import FilmsApiService from './api-content';
-
 import filmCards from '../templates/mylibrary.hbs';
-
 import { checkIfEmptyLibrary } from './header-observer';
-
-// var pagination = require('./maryska');
-// import PaginationMyLibrary from './pagination-for-my-library';
-// const paginationMyLibrary = new PaginationMyLibrary();
-// console.log(paginationMyLibrary);
 import {paginationMyLibrary}from '../index';
-
-
-
 //* экземпляр класса API
 const newFilmsApi = new FilmsApiService();
 
 const { bodyEl, cardsList, myLibraryButton, wBtn, qBtn, homeButton} = refs;
  //* поиск id фильма
-bodyEl.addEventListener('click', addToWatched); //* поиск и добавление в Watched
-bodyEl.addEventListener('click', addToQueue); //* поиск и добавление в Queue
+bodyEl.addEventListener('click', testWhatButtonIsIt);
 wBtn.addEventListener('click', renderWatched);
 qBtn.addEventListener('click', renderQueue);
-homeButton.addEventListener('click', removeAnimation);
 myLibraryButton.addEventListener('click', onMyLibraryButtonClick)
 
 // let liId = null;
@@ -30,7 +18,13 @@ let localStorageWatched = localStorage.getItem('watched') ? localStorage.getItem
 let localStorageQueue = localStorage.getItem('queue') ? localStorage.getItem('queue') : '';  //* local storage queue
 export let library = [];
 let arrOfIds = [];
-
+function testWhatButtonIsIt(e) {
+    if (e.target.dataset.modal === 'watched') {
+        return addToWatched(e);
+    }else if (e.target.dataset.modal === 'queue') {
+        return addToQueue(e);
+    }else{return;}
+}
 function addArrOfIds(nameIds) {
   return  arrOfIds = localStorage.getItem(`${nameIds}`) ? JSON.parse(localStorage.getItem(`${nameIds}`)) : [];
 
@@ -45,9 +39,6 @@ function getId(ev) {
 //* добавляем id фильма в WATCHED (localStorageWatched)
 function addToWatched(e) {
     const nameIds = 'watched';
-    if (e.target.dataset.modal !== 'watched') {
-        return;
-    }
     const liId = getId(e)
     addArrOfIds(nameIds)
     if (arrOfIds.includes(liId)) {
@@ -56,15 +47,12 @@ function addToWatched(e) {
 
     arrOfIds.push(liId);
     localStorage.setItem('watched', JSON.stringify(arrOfIds));
+    // addCurrentOnButton(e)
 }
 
 //* добавляем id фильма в QUEUE (localStorageQueue)
 function addToQueue(e) {
     const nameIds = 'queue'
-    if (e.target.dataset.modal !== 'queue') {
-        return;
-    }
-    console.log(e.target.dataset.action);
     const liId = getId(e)
     addArrOfIds(nameIds)
     if (arrOfIds.includes(liId)) {
@@ -75,48 +63,35 @@ function addToQueue(e) {
     localStorage.setItem('queue', JSON.stringify(arrOfIds));
 }
 
-function startPagination(arrayIds) {
-    library =[];
-    const promises = arrayIds.map(el => {
-        return  newFilmsApi.fetchInformationAboutFilm(el).then(addLibrary)
-
-    });
-    Promise.all(promises).then(makePagin);
-}
-
 //* Функция рендера списка Watched
 async function renderWatched(e) {
-    cardsList.innerHTML = "";
+    addCurrentOnButton(e)
+    const nameIds = 'watched';
     e.preventDefault();
-
-    const localArr = addArrOfIds();
+    
+    const localArr = addArrOfIds(nameIds);
 
     paginationMyLibrary.startPagination(localArr);
-    cardsList.classList.add('animation');
+
 }
 
 
 //* Функция рендера списка Queue
 async function renderQueue(e) {
-    console.log(localStorage.getItem('queue').split(','))
-    cardsList.innerHTML = "";
+
+    const nameIds = 'queue'
 
     e.preventDefault();
-    const localArr = localStorage.getItem('queue').split(',');
+    const localArr = addArrOfIds(nameIds);
+    paginationMyLibrary.startPagination(localArr);
 
-    startPagination(localArr);
 
-    cardsList.classList.add('animation');
 }
 
-// async function addLibrary(film) {
-//    await library.push(film);
-//    return await film;
-// }
-
 function onMyLibraryButtonClick(e) {
-    const allIds = [...addArrOfIds('queue'), ...addArrOfIds('watched')]
+    const allIds = [...addArrOfIds('queue'), ...addArrOfIds('watched')];
     paginationMyLibrary.startPagination(allIds);
+
 }
 
 // let splitItem; //* test
@@ -130,16 +105,6 @@ function onMyLibraryButtonClick(e) {
 //         console.log(localStorageArr);
 
 
-//         // localStorage.getItem('watched').split(',').forEach(function(el, index){
-//         //     if (el === liId) { //! как найти liId через forEach?
-//         //         localStorage.getItem('watched').split(',').splice(index, 1);
-//         //         console.log('proshlo!', localStorage.getItem('watched').split(','))
-//         //     }
-//         // });
-//     }
-// }
-
-
 
 // //* удаление id из очереди
 // function deleteQueueFilm() {
@@ -149,39 +114,11 @@ function onMyLibraryButtonClick(e) {
 
 
 //* удаление класса анимации, чтоб она повторялась
-function removeAnimation() {
-    cardsList.classList.remove('animation');
+function addCurrentOnButton(e) {
+    e.target.parentNode.classList.add('current');
+    // e.target.classList.add('current');
+    // e.target.cardsList.add('current');
 }
-
-
-
-
-//* pagination
-// var pag3;
-// var itemsPerPage = 20;
-// export function makePagin() {
-//     // console.log(library)
-//      pag3 = new pagination(document.getElementsByClassName('pagination')[0],
-//   {
-//       currentPage: 1,		// number
-//       totalItems: library.length,       // number
-//       itemsPerPage: 20,    // number
-//       stepNum: 2,			// number
-//       onInit: renderLibrary	        // function
-//   });
-//    pag3.onPageChanged(renderLibrary);
-// }
-
-// function renderLibrary(page) {
-//     refs.cardsList.innerHTML = renderNextLibrary(page);
-// }
-
-// function renderNextLibrary(currentPage) {
-//         let template = "";
-//     for (let i = (currentPage - 1) * itemsPerPage; i < (currentPage * itemsPerPage) && i < library.length; i++) {
-
-//         template += filmCards(library[i]);
-//     }
-    
-//     return template;
-// }
+function removeCurrentOnButton(e) {
+    e.target.cardsList.remove('current');
+}
