0fab3cfc7a069af676e175189ee8688568786aef
Ivan Laver <ereyzer142@gmail.com>
2021-06-30 15:54:17 +0300

    fix

--- src/js/annoyment.js
+++ src/js/annoyment.js
@@ -1,6 +1,4 @@
 import { refs } from "./variables";
-import { signInUser } from './autentification';
-// console.log('Oksana', identif);
 
 const { annoymentModal, closeAnnoyment, overlay } = refs;
 
@@ -11,22 +9,7 @@ let userSubscribed = false;
 export let timerCloseModal = null;
 export let timerOpenModal = null;
 let counterOpenedModal = 0;
-// const identif = false;
-// function rewrite(res) {
-//    return identif = res
-// }
-    //  if (identif) {
-    //      console.log(identif)
-    // userSubscribed = true
-    //  } else {
-    //      checkingTheOpeningCondition()
-    //  }
-    // async function authentication() {
-    //     const ifAuthentication = await signInUser();
 
-    //     // return await ifAuthentication().then(console.log)
-    // }
-    // authentication()
 export function checkingTheOpeningCondition() {
     if (counterOpenedModal >= MAX_PROMT_MODAL || userSubscribed === true) {
         return;
